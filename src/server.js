7d435d223199e5a12be00310c8f6ba46079ebd7b
José Hernández <jhernanq@everis.com>
2021-04-13 15:22:04 -0400

    master
    test de depliegue

---
+++ src/server.js
@@ -0,0 +1,8 @@
+const app = express();
+const PORT = process.env.PORT || 3050;
+
+app.use(express.static(__dirname + '/dist/crudLoginAngular'));
+app.get('/', function (req, res) {
+  res.sendFile(path.join(__dirname + '/dist/crudLoginAngular/index.html'));
+});
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
